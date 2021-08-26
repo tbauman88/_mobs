104294473719028c1844f6e69af1b1db5d164eb0
Tyler Bauman <t.bauman@vehikl.com>
2021-08-26 11:58:28 -0400

    Use data from api to render cards

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -35,7 +35,15 @@ import './Tab1.scss'
 import axios from 'axios'
 import { peopleOutline } from 'ionicons/icons'
 import { useState } from 'react'
-import api from '../api'
+import { DateTime } from "luxon";
+
+const convertTimeDiff = (startTime: string, endTime: string) => {
+  let start = DateTime.fromISO(startTime) 
+  let end = DateTime.fromISO(endTime) 
+  // return end.diff(start, 'hours')
+  console.log(start, end);
+  return '{start, end}'
+}
 
 const Tab1: React.FC = () => {
   const [view, setView] = useState<string | undefined>('day')
@@ -67,7 +75,8 @@ const Tab1: React.FC = () => {
 
   const sessions = Object.keys(data?.data).map((key) => [...data?.data[key]])
 
-  console.log(sessions)
+  console.log(data?.data)
+  console.log(sessions, 'session')
 
   return (
     <IonPage>
@@ -91,96 +100,114 @@ const Tab1: React.FC = () => {
           </IonSegmentButton>
         </IonSegment>
         {view === 'day' && (
-          <IonCard className="ion-no-margin">
-            <IonList inset mode="ios">
-              <IonItem lines="none" className="ion-align-items-start ion-no-padding">
-                <IonThumbnail className="thumbnail" slot="start">
-                  <IonText color="light">
-                    <h3>11:30</h3>
-                  </IonText>
-                  <IonText color="light">AM</IonText>
-                </IonThumbnail>
-                <div className="ion-text-wrap">
-                  <IonGrid>
-                    <IonRow className="ion-margin-bottom">
-                      <IonText color="dark">
-                        <h1 className="ion-no-margin">
-                          Finishing off our multi-group dropdown menu
-                        </h1>
-                      </IonText>
-                    </IonRow>
-                    <IonRow className="ion-margin-bottom ion-align-items-center">
-                      <IonText className="ion-margin-end" color="medium">
-                        <h5 className="ion-no-margin">Darren Galway</h5>
-                      </IonText>
-                      <IonAvatar>
-                        <img src="https://avatars.githubusercontent.com/u/11524317?s=96&v=4" />
-                      </IonAvatar>
-                    </IonRow>
-                    <IonRow className="ion-margin-bottom">
-                      <IonChip color="dark">
-                        <IonLabel color="medium">Private</IonLabel>
-                      </IonChip>
-                      <IonChip color="dark">
-                        <IonLabel color="medium">1 hour</IonLabel>
-                      </IonChip>
-                    </IonRow>
-                    <IonRow className="ion-align-items-center">
-                      <IonCol>
-                        <IonIcon color="medium" icon={peopleOutline}></IonIcon>
-                        <IonText color="medium">4 spots remaining</IonText>
-                      </IonCol>
-                      <IonCol className="ion-align-self-end">
-                        <IonButton size="default" fill="clear" color="primary">
-                          Join
-                        </IonButton>
-                      </IonCol>
-                    </IonRow>
-                  </IonGrid>
-                </div>
-              </IonItem>
-            </IonList>
-          </IonCard>
+          <>
+            {sessions.map((day, index) =>
+              day.map((session) => (
+                <>
+                  {session.date === date.toISOString().split('T')[0] && (
+                    <IonCard className="ion-no-margin">
+                      <IonList inset mode="ios">
+                        <IonItem lines="none" className="ion-align-items-start ion-no-padding">
+                          <IonThumbnail className="thumbnail" slot="start">
+                            <IonText color="light">
+                              <h3>{session.start_time.split(" ")[0]}</h3>
+                            </IonText>
+                            <IonText color="light">{session.start_time.split(" ")[1]}</IonText>
+                          </IonThumbnail>
+                          <div className="ion-text-wrap">
+                            <IonGrid>
+                              <IonRow className="ion-margin-bottom">
+                                <IonText color="dark">
+                                  <h1 className="ion-no-margin">
+                                    {session.title}
+                                  </h1>
+                                </IonText>
+                              </IonRow>
+                              <IonRow className="ion-margin-bottom ion-align-items-center">
+                                <IonText className="ion-margin-end" color="medium">
+                                  <h5 className="ion-no-margin">{session.owner.name}</h5>
+                                </IonText>
+                                <IonAvatar>
+                                  <img src={session.owner.avatar} alt={session.owner.name} />
+                                </IonAvatar>
+                              </IonRow>
+                              <IonRow className="ion-margin-bottom">
+                                <IonChip color="dark">
+                                  <IonLabel color="medium">
+                                    {session.is_public ? 'Public' : 'Private'}
+                                  </IonLabel>
+                                </IonChip>
+                                <IonChip color="dark">
+                                  <IonLabel color="medium">
+                                    {
+                                      convertTimeDiff(session.start_time, session.end_time)
+                                    }
+                                  </IonLabel>
+                                </IonChip>
+                              </IonRow>
+                              <IonRow className="ion-align-items-center">
+                                <IonCol>
+                                  <IonIcon color="medium" icon={peopleOutline}></IonIcon>
+                                  <IonText color="medium">
+                                    {session.attendee_limit - session.attendees.length} spots remaining
+                                  </IonText>
+                                </IonCol>
+                                <IonCol className="ion-text-end">
+                                  <IonButton size="default" fill="clear" color="primary">
+                                    Join
+                                  </IonButton>
+                                </IonCol>
+                              </IonRow>
+                            </IonGrid>
+                          </div>
+                        </IonItem>
+                      </IonList>
+                    </IonCard>
+                  )}
+                </>
+              ))
+            )}
+          </>
         )}
         {view === 'week' && (
           <IonGrid>
             <IonRow>
-              <IonCol>
-              Monday
-                <IonCard>
-                  <IonCardHeader>
-                    <IonCardTitle>Title</IonCardTitle>
-                  </IonCardHeader>
-                  <IonCardContent>
-                    <IonItem lines="none">
-                      <p>Darren Galway</p>
-                      <IonAvatar>
-                        <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
-                      </IonAvatar>
-                    </IonItem>
-                    <IonItem lines="none">
-                      <IonChip>
-                        <IonLabel>Private</IonLabel>
-                      </IonChip>
-                      <IonChip>
-                        <IonLabel>1 hour</IonLabel>
-                      </IonChip>
-                    </IonItem>
-                    <IonItem lines="none">
-                      <IonLabel>
-                        <IonIcon icon={peopleOutline}></IonIcon>4 spots remaining
-                      </IonLabel>
-                      <IonNote slot="end" color="tertiary">
-                        Join
-                      </IonNote>
-                    </IonItem>
-                  </IonCardContent>
-                </IonCard>
-              </IonCol>
-              <IonCol>Tuesday</IonCol>
-              <IonCol>Wednesday</IonCol>
-              <IonCol>Thursday</IonCol>
-              <IonCol>Friday</IonCol>
+              {sessions.map((day, index) => (
+                <IonCol key={index}>
+                  {day.map((session) => (
+                    <IonCard key={session.id}>
+                      <IonCardHeader>
+                        <IonCardTitle>{session.title}</IonCardTitle>
+                      </IonCardHeader>
+                      <IonCardContent>
+                        <IonItem lines="none">
+                          <p>{session.owner.name}</p>
+                          <IonAvatar>
+                            <img src={session.owner.avatar} alt={session.owner.name} />
+                          </IonAvatar>
+                        </IonItem>
+                        <IonItem lines="none">
+                          <IonChip>
+                            <IonLabel>{session.is_public ? 'Public' : 'private'}</IonLabel>
+                          </IonChip>
+                          <IonChip>
+                            <IonLabel>1 hour</IonLabel>
+                          </IonChip>
+                        </IonItem>
+                        <IonItem lines="none">
+                          <IonLabel>
+                            <IonIcon icon={peopleOutline}></IonIcon>
+                            {session.attendee_limit - session.attendees.length}
+                          </IonLabel>
+                          <IonNote slot="end" color="tertiary">
+                            Join
+                          </IonNote>
+                        </IonItem>
+                      </IonCardContent>
+                    </IonCard>
+                  ))}
+                </IonCol>
+              ))}
             </IonRow>
           </IonGrid>
         )}
